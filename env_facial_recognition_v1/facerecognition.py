991cecf15a310928a0bb814f498f6141dc6cb479
Amornpan Phornchaicharoen <amornpan@gmail.com>
2019-08-21 22:50:35 +0700

    add sqlite3 and optimize code

--- env_facial_recognition_v1/facerecognition.py
+++ env_facial_recognition_v1/facerecognition.py
@@ -5,26 +5,8 @@ from PIL import ImageFile
 import face_recognition_models
 import cv2
 import face_recognition
+import sqlite3
 
-
-ImageFile.LOAD_TRUNCATED_IMAGES = True
-
-face_detector = dlib.get_frontal_face_detector()
-
-predictor_68_point_model = face_recognition_models.pose_predictor_model_location()
-pose_predictor_68_point = dlib.shape_predictor(predictor_68_point_model)
-
-predictor_5_point_model = face_recognition_models.pose_predictor_five_point_model_location()
-pose_predictor_5_point = dlib.shape_predictor(predictor_5_point_model)
-
-cnn_face_detection_model = face_recognition_models.cnn_face_detector_model_location()
-cnn_face_detector = dlib.cnn_face_detection_model_v1(cnn_face_detection_model)
-
-face_recognition_model = face_recognition_models.face_recognition_model_location()
-face_encoder = dlib.face_recognition_model_v1(face_recognition_model)
-
-
-print(cv2.__version__)
 
 # Get a reference to webcam #0 (the default one)
 video_capture = cv2.VideoCapture(0)
