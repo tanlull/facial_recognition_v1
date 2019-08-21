9ba63152fbd122aa7c38b3454d3ff5df5e659712
Amornpan Phornchaicharoen <amornpan@gmail.com>
2019-08-21 18:13:41 +0700

    intitial package cv2

---
+++ env_facial_recognition_v1/facerecognition.py
@@ -0,0 +1,25 @@
+import PIL.Image
+import dlib
+import numpy as np
+from PIL import ImageFile
+import face_recognition_models
+import cv2
+
+
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+face_detector = dlib.get_frontal_face_detector()
+
+predictor_68_point_model = face_recognition_models.pose_predictor_model_location()
+pose_predictor_68_point = dlib.shape_predictor(predictor_68_point_model)
+
+predictor_5_point_model = face_recognition_models.pose_predictor_five_point_model_location()
+pose_predictor_5_point = dlib.shape_predictor(predictor_5_point_model)
+
+cnn_face_detection_model = face_recognition_models.cnn_face_detector_model_location()
+cnn_face_detector = dlib.cnn_face_detection_model_v1(cnn_face_detection_model)
+
+face_recognition_model = face_recognition_models.face_recognition_model_location()
+face_encoder = dlib.face_recognition_model_v1(face_recognition_model)
+
+print(cv2.__version__)
